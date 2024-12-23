d2119481082e673400cb5d0e018293b5a5f7bd2a
BILAL ZOUINE <bilalzouine9@gmail.com>
2024-12-23 21:01:00 +0100

    update file frontend/src/pages/login.jsx and call commponent studentLogin.jsx

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import StudentLogin from '../components/student/StudentLogin';
 function Login() {
     return (
-        <h1> Login</h1>
+        <StudentLogin/>
     );
 }
 
