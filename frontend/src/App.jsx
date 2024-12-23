c23129fbd0f26a3fb887b40e2b8c4529661cf29c
BILAL ZOUINE <bilalzouine9@gmail.com>
2024-12-23 18:55:24 +0100

    Setup frontend components, services, and styling updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import { router } from './route/index.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-    
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </>
   )
 }
