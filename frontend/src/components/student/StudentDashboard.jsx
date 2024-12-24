406c5f5bafdc3ad06ad1207463cff445ad6c0c37
BILAL ZOUINE <bilalzouine9@gmail.com>
2024-12-24 20:55:31 +0100

    fix: prevent access to table info if student is null or empty
    - Updated `StudentDashboard.jsx` to check if the `student` object is `null` or empty.
    - Added a conditional rendering mechanism to hide table information for unauthenticated or uninitialized students.
    - Improved user experience by ensuring proper data validation before rendering sensitive information.

--- frontend/src/components/student/StudentDashboard.jsx
+++ frontend/src/components/student/StudentDashboard.jsx
@@ -7,45 +7,47 @@ import { STUDENT_LOGIN_ROUTER } from '../../route';
 function StudentDashboard() {
 
     const { student } = useStudentContext()
+    const isObjectEmpty = Object.keys(student).length;
 
     return (
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th scope="col" className="px-6 py-3">
-                        #ID
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                        Full Name
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                        Email
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                        Date create account
-                    </th>
-
-                </tr>
-            </thead>
-            <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {student.id}
-                    </th>
-                    <th className="px-6 py-4 ">
-                        {student.name}
-                    </th>
-                    <td className="px-6 py-4">
-                        {student.email}
-                    </td>
-                    <td className="px-6 py-4">
-                        {student.created_at}
-                    </td>
-
-                </tr>
-
-            </tbody>
-        </table>
+        <>
+            {
+                isObjectEmpty > 0 && <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                #ID
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Full Name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Email
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Date create account
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {student.id}
+                            </th>
+                            <th className="px-6 py-4 ">
+                                {student.name}
+                            </th>
+                            <td className="px-6 py-4">
+                                {student.email}
+                            </td>
+                            <td className="px-6 py-4">
+                                {student.created_at}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            }
+        </>
     );
 }
 
