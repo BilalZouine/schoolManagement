deafb8de761c765d29a4b9500e62e1615a2876ae
BILAL ZOUINE <bilalzouine9@gmail.com>
2024-12-30 13:10:22 +0100

    update all navbar layout

---
+++ frontend/src/layouts/StudentDropdownMenu.jsx
@@ -0,0 +1,88 @@
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { LogOut } from "lucide-react"
+import { useStudentContext } from "../contexts/studentContext"
+
+export function StudentDropdownMenu() {
+  const { logout: contextLogout ,student} = useStudentContext()
+
+  const logout = () => {
+    contextLogout()
+    navigate(STUDENT_LOGIN_ROUTER);
+  }
+ 
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="px-3 h-8 hover:h-9" >
+          {student.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Keyboard shortcuts
+            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>Email</DropdownMenuItem>
+                <DropdownMenuItem>Message</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>More...</DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuItem>
+            New Team
+            <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>GitHub</DropdownMenuItem>
+        <DropdownMenuItem>Support</DropdownMenuItem>
+        <DropdownMenuItem disabled>API</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={logout}>
+          <LogOut />
+          <apan>log out</apan>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
---
+++ frontend/src/layouts/studentDashbordLayoute.jsx
@@ -0,0 +1,88 @@
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { HOME_ROUTER, STUDENT_DASHBOARD_ROUTER, STUDENT_LOGIN_ROUTER, STUDENTS_ROUTER } from '../route';
+// import { HOME_PAGE_ROUTER, STUDENT_DASHBOARD_ROUTER, STUDENT_LOGIN_ROUTER, STUDENT_LOGOUT_ROUTER, STUDENT_REGISTER_ROUTER, STUDENTS_ROUTER } from '../router';
+import { Button } from '@/components/ui/button'
+import { useStudentContext } from '../contexts/studentContext';
+import { useEffect } from 'react';
+import { StudentApi } from '../service/api/student/studentApi';
+import { Gauge, Home, LogOut, UserCircle2 } from 'lucide-react';
+import { StudentDropdownMenu } from './StudentDropdownMenu';
+import { ModeToggle } from '../components/mode-toggle';
+function StudentDashboardLayout() {
+    const { setStudent, setAuthenticated } = useStudentContext()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        // if(!authenticated){
+        //     navigate(STUDENT_LOGIN_ROUTER)
+        //     return;
+        // }
+        StudentApi.getStudent()
+            .then((data) => {
+                setStudent(data.data)
+                setAuthenticated(true)
+            })
+            .catch((reasen) => {
+                contextLogout(false)
+                navigate(STUDENT_LOGIN_ROUTER); // Redirect to the login page
+            })
+    }, []);
+
+
+    return (
+        <>
+            <header className="bg-white shadow-sm">
+                <div className="mx-auto flex justify-between items-center px-12 py-2 mb-4 shadow-1xl">
+                    {/* Logo */}
+                    <div className="text-2xl text-white font-semibold inline-flex items-center">
+                        <img className='h-12 mx-2' src="logo.png" alt="" />
+                    </div>
+                    {/* Desktop Menu Links */}
+                    <ul className="flex justify-evenly items-center w-1/3  ">
+                        <li>
+                            <Link
+                                to={HOME_ROUTER}
+                                className="text-decoration-none"
+                            >
+                                <Home />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to={STUDENT_DASHBOARD_ROUTER}
+                                className="text-decoration-none"
+                            >
+                                <Gauge />
+
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to={STUDENTS_ROUTER}
+                                className="text-decoration-none"
+                            >
+                                <UserCircle2 />
+                            </Link>
+                        </li>
+
+                        <li>
+                            <StudentDropdownMenu />
+                        </li>
+                        <li>
+                            <ModeToggle />
+                        </li>
+                    </ul>
+                </div>
+            </header>
+
+            <main className="container mx-auto px-4 sm:px-6 lg:px-8">
+                <Outlet />
+
+            </main>
+
+            <footer></footer>
+        </>
+    );
+}
+
+export default StudentDashboardLayout;
